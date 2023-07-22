d21a6155f659fd2ef1febd4fec20112dbee340f7
Sam Lu <pbcmarsh@gmail.com>
2023-07-22 15:46:33 -0500

    Create Two Number Sum-Solution3.java

---
+++ Java/Two Number Sum-Solution3.java
@@ -0,0 +1,28 @@
+import java.util.*;
+// Time Complexity: O(nlogn)
+// Space Complexity: O(1)
+// Solution 3: Sort and Pointers
+class Program {
+  public static int[] twoNumberSum(int[] array, int targetSum) {
+    // Sort array
+    Arrays.sort(array);
+    // Left and right pointer
+    int l = 0;
+    int r = array.length - 1;
+    while (l < r) {
+      // Check if two pointers sum match targetSum
+      if (array[l] + array[r] < targetSum) {
+        // Sum too small, find higher left value
+        l++;
+      } else if (array[l] + array[r] > targetSum) {
+        // Sum too large, find smaller right value
+        r--;
+      } else {
+        // Sum found, return array of two number sum
+        return new int[]{array[l], array[r]};
+      }
+    }
+    // No such two number sum
+    return new int[0];
+  }
+}
