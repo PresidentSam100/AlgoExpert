34edd4ad3760aedbafe161eb5c5e3a57bdd510b0
Sam Lu <pbcmarsh@gmail.com>
2023-07-22 15:37:38 -0500

    Create Two Number Sum-Solution2.java

---
+++ Java/Two Number Sum-Solution2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+// Solution 2: Hashing
+class Program {
+  public static int[] twoNumberSum(int[] array, int targetSum) {
+    Set<Integer> table = new HashSet<>();
+    // Go through entire array
+    for (int num : array) {
+      // Check if num has matching pair in table
+      if (table.contains(targetSum - num)) {
+        return new int[]{num, targetSum - num};
+      }
+      else {
+        table.add(num);
+      }
+    }
+    return new int[0];
+  }
+}
