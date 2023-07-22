45746b8c9d200aee3a180b0b8ca00cf5cf2ac508
Sam Lu <pbcmarsh@gmail.com>
2023-07-22 16:05:46 -0500

    Create Sorted Squared Array-Solution1.java

---
+++ Java/Sorted Squared Array-Solution1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+// Time Complexity: O(nlogn)
+// Space Complexity: O(n)
+// Solution 1: Square and Sort
+class Program {
+
+  public int[] sortedSquaredArray(int[] array) {
+    // New array to return
+    int[] arraySquare = new int[array.length];
+    // Take square of every element in array and store it
+    for (int i = 0; i < array.length; i++) {
+      arraySquare[i] = array[i] * array[i];
+    }
+    // Sort array (accounts for negative values in original array)
+    Arrays.sort(arraySquare);
+    return arraySquare;
+  }
+}
