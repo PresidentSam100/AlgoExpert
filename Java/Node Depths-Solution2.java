9f0314e59b5bcf0f2073af41ed9b17c962b18822
Sam Lu <pbcmarsh@gmail.com>
2023-07-25 18:30:39 -0500

    Create Node Depths-Solution2.java

---
+++ Java/Node Depths-Solution2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+// Average Time Complexity: O(n)
+// Average Space Complexity: O(h)
+// n is the number of nodes in the Binary Tree
+// h is the height of the Binary Tree
+// Solution: Double Loop and Transpose Operation
+class Program {
+
+  public static int nodeDepths(BinaryTree root) {
+    return nodeDepthsHelper(root, 0);
+  }
+
+  public static int nodeDepthsHelper(BinaryTree root, int depth) {
+    if (root == null) {
+      return 0;
+    }
+    return depth + nodeDepthsHelper(root.left, depth + 1) + nodeDepthsHelper(root.right, depth + 1);
+  }
+  
+  static class BinaryTree {
+    int value;
+    BinaryTree left;
+    BinaryTree right;
+
+    public BinaryTree(int value) {
+      this.value = value;
+      left = null;
+      right = null;
+    }
+  }
+}
