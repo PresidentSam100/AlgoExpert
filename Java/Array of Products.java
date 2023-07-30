ab9346f8287b5b5b519040962f89e230f867aef5
Sam Lu <pbcmarsh@gmail.com>
2023-07-30 17:22:16 -0500

    Create Array of Products.java

---
+++ Java/Array of Products.java
@@ -0,0 +1,20 @@
+import java.util.*;
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+// n is the length of the array
+// Solution 1: Brute Force
+class Program {
+  public int[] arrayOfProducts(int[] array) {
+    int n = array.length;
+    int[] products = new int[n];
+    Arrays.fill(products, 1);
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i != j) {
+          products[i] *= array[j];
+        }
+      }
+    }
+    return products;
+  }
+}
