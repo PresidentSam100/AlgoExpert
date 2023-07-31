fd1173bf97fb5f2a77fb676c9c625020491dc8a4
Sam Lu <pbcmarsh@gmail.com>
2023-07-30 19:37:50 -0500

    Problem Details

--- Java/First Non-Repeating Character-Solution1.java
+++ Java/First Non-Repeating Character-Solution1.java
@@ -1,5 +1,8 @@
 import java.util.*;
-
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+// n is the number of characters in string
+// Solution 1: Brute Force double loop
 class Program {
   public int firstNonRepeatingCharacter(String string) {
     for (int i = 0; i < string.length(); i++) {
