ec9dc38e3360d39a39a93f7ae8e9d6eba18c3970
Sam Lu <pbcmarsh@gmail.com>
2023-07-31 21:24:13 -0500

    Time/Space Complexity

--- Java/Nth Fibonacci.java
+++ Java/Nth Fibonacci.java
@@ -1,5 +1,8 @@
 import java.util.*;
-
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// n is the nth fibonacci value
+// Solution: Two variables and sum
 class Program {
   public static int getNthFib(int n) {
     int a = 0;
