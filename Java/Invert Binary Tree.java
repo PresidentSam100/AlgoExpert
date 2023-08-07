cbcf33ffeb10c4081f82977f205b8ed18622297b
Sam Lu <pbcmarsh@gmail.com>
2023-08-07 14:41:36 -0500

    Create Invert Binary Tree.java

---
+++ Java/Invert Binary Tree.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Program {
+  public static void invertBinaryTree(BinaryTree tree) {
+    if (tree != null) {
+      BinaryTree temp = tree.left;
+      tree.left = tree.right;
+      tree.right = temp;
+      invertBinaryTree(tree.left);
+      invertBinaryTree(tree.right);
+    }
+  }
+
+  static class BinaryTree {
+    public int value;
+    public BinaryTree left;
+    public BinaryTree right;
+
+    public BinaryTree(int value) {
+      this.value = value;
+    }
+  }
+}
