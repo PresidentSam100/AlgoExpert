c0d7bb75fc035507b399b9e57b47fef375d0c0a0
Sam Lu <pbcmarsh@gmail.com>
2023-08-07 10:16:48 -0500

    Update Remove Duplicates From Linked List.java

--- Java/Remove Duplicates From Linked List.java
+++ Java/Remove Duplicates From Linked List.java
@@ -1,5 +1,8 @@
 import java.util.*;
-
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// n is the number of nodes in the LinkedList
+// Solution: Iterate and Check for Duplicates
 class Program {
   // This is an input class. Do not edit.
   public static class LinkedList {
