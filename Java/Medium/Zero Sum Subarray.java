d3a5fd805529883a92d259d8bb95593ed4ad68b4
Sam Lu <pbcmarsh@gmail.com>
2023-10-15 15:06:06 -0500

    comments

--- Java/Medium/Zero Sum Subarray.java
+++ Java/Medium/Zero Sum Subarray.java
@@ -9,9 +9,11 @@ class Program {
     Set<Integer> prefix = new HashSet<>();
     int prev = 0;
     for (int i = 0; i < n; i++) {
+      // Check if current sum is 0 or has already been in prefix
       if (prev + nums[i] == 0 || prefix.contains(prev + nums[i])) {
           return true;
       } else {
+        // Add prefix sum to set
         prefix.add(prev + nums[i]);
         prev += nums[i];
       }
