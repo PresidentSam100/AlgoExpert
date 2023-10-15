95422a7e446e0f7165a3a7f72b9a7cf2e7e2ef80
Sam Lu <pbcmarsh@gmail.com>
2023-10-15 15:55:52 -0500

    update to not modify dishes parameter

--- Java/Medium/Sweet And Savory.java
+++ Java/Medium/Sweet And Savory.java
@@ -1,27 +1,28 @@
 import java.util.*;
 // Time Complexity: O(nlogn)
-// Space Complexity: O(1)
+// Space Complexity: O(n)
 // n is the total number of dishes
 // Solution: Sort and Two Pointers and Conditionals
 class Program {
   public int[] sweetAndSavory(int[] dishes, int target) {
     // Sort dishes in ascending order
-    Arrays.sort(dishes);
+    int[] dishesTemp = dishes.clone();
+    Arrays.sort(dishesTemp);
     // Two pointers for dish pairing
     int left = 0;
-    int right = dishes.length - 1;
+    int right = dishesTemp.length - 1;
     int[] ans = {0, 0};
     int curr = Integer.MIN_VALUE;
     while (left < right) {
       // Dishes don't have opposite flavor profiles
-      if (dishes[left] >= 0 || dishes[right] <= 0) {
+      if (dishesTemp[left] >= 0 || dishesTemp[right] <= 0) {
         break;
       }
-      int flavor = dishes[left] + dishes[right];
+      int flavor = dishesTemp[left] + dishesTemp[right];
       if (flavor < target) {
         // New best target combined flavor profile
         if (curr < flavor) {
-          ans = new int[]{dishes[left], dishes[right]};
+          ans = new int[]{dishesTemp[left], dishesTemp[right]};
           curr = flavor;
         }        
         // Check for less less sweet dish
@@ -31,7 +32,7 @@ class Program {
         right--;
       } else {
         // Target combined flavor profile reached
-        return new int[]{dishes[left], dishes[right]};
+        return new int[]{dishesTemp[left], dishesTemp[right]};
       }
     }
     return ans;
